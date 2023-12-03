4c6ad0e7e5febb7af17be83429f5ac77b0848f8a
vinicius <viniolvs.dev@gmail.com>
2023-12-03 10:59:07 -0300

    add: dist

--- src/models/Point.js
+++
@@ -1,7 +0,0 @@
-export default class Point {
-  constructor(x, y, color) {
-    this.x = x
-    this.y = y
-    this.color = color
-  }
-}
--- src/models/Poly.js
+++
@@ -1,7 +0,0 @@
-export default class Poly {
-  constructor(name, points, color) {
-    this.name = name
-    this.points = points
-    this.edgeColor = color
-  }
-}
