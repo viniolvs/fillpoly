d7f2a8d579c93dc2089eacb811bfa2bc1fda9f8d
Vinicius de Oliveira <viniolvs.dev@gmail.com>
2024-10-06 14:03:11 -0300

    copy project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import  Area  from './components/Area.jsx'
+
+function App() {
+  return (
+    <>
+      <Area/>
+    </>
+  )
+}
+
+export default App
---
+++ src/models/Poly.js
@@ -0,0 +1,7 @@
+export default class Poly {
+  constructor(name, points, color) {
+    this.name = name
+    this.points = points
+    this.edgeColor = color
+  }
+}
